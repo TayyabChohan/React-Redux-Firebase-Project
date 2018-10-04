df4c4c5cf4932f2b346a47eb5f519e203cbedb3d
Tayyab Chohahn <tayyabchohan7@gmail.com>
2018-10-04 22:25:15 +0500

    SrollToTop Solution

--- src/index.js
+++ src/index.js
@@ -7,13 +7,16 @@ import registerServiceWorker from "./registerServiceWorker";
 import "semantic-ui-css/semantic.min.css";
 import { Provider } from "react-redux";
 import { configureStore } from "./app/store/configureStore";
+import SrollToTop from './app/common/util/srollToTop'
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+    <SrollToTop>
       <App />
+      </SrollToTop>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
