95e3e25f27a9f2f1fe940b87453478e8d803dd85
Tayyab Chohahn <tayyabchohan7@gmail.com>
2018-10-04 17:03:20 +0500

    EventDetailPage

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import {BrowserRouter} from 'react-router-dom'
-import App from './app/layout/App.jsx';
-import registerServiceWorker from './registerServiceWorker';
-import  'semantic-ui-css/semantic.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./app/layout/App.jsx";
+import registerServiceWorker from "./registerServiceWorker";
+import "semantic-ui-css/semantic.min.css";
+import { Provider } from "react-redux";
+import { configureStore } from "./app/store/configureStore";
+
+const store = configureStore();
 
 ReactDOM.render(
-<BrowserRouter>
-
-<App/>
-</BrowserRouter>
-   , document.getElementById('root'));
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
