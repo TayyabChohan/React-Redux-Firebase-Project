2d9909005ccc71b01e52c8a9169d42137066d714
Tayyab Chohahn <tayyabchohan7@gmail.com>
2018-10-22 03:44:34 +0500

    Hooking the update user method to the form

--- src/app/common/form/DateInput.jsx
+++ src/app/common/form/DateInput.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
-import { Form, label } from 'semantic-ui-react'
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
-import moment from 'moment'
-const DateInput = ( {input:{value, onChange, ...restInput}, width, placeholder, meta:{touched, error},...rest} ) => {
+import React from "react";
+import { Form, label } from "semantic-ui-react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import moment from "moment";
+const DateInput = ({
+  input: { value, onChange, ...restInput },
+  width,
+  placeholder,onBlur,
+  meta: { touched, error },
+  ...rest
+}) => {
+  if(value){
+    value=moment(value, 'X')
+  }
   return (
-    
-    <Form.Field error={touched && !! error} width={width} >
-    <DatePicker
-    {...rest}
-    placeholderText={placeholder}
-    selected={value ? moment(value) :null}
-    onChange={onChange}
-    {...restInput}
+    <Form.Field error={touched && !!error} width={width}>
+      <DatePicker
+        {...rest}
+        placeholderText={placeholder}
+        selected={value ? moment(value) : null}
+        onChange={onChange}
+        onBlur={()=>onBlur()}
+        {...restInput}
+      />
+      {touched &&
+        error && (
+          <label basic color="red">
+            {error}{" "}
+          </label>
+        )}
+    </Form.Field>
+  );
+};
 
-    />
-     { touched && error && <label basic color='red'>{error} </label>}
-    </Form.Field> 
-    
-  )
-}
-
-export default DateInput
+export default DateInput;
--- src/features/user/Settings/SettingDashBoard.jsx
+++ src/features/user/Settings/SettingDashBoard.jsx
@@ -8,21 +8,23 @@ import AcountPage from "./AcountPage";
 import { connect } from "react-redux";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { updatePassword } from "../../auth/AuthActions";
+import { updateProfile } from '../../user/userAction'
 const actions = {
-  updatePassword
+  updatePassword,
+  updateProfile
 };
 const mapState=(state)=>({
    providerId: state.firebase.auth.providerData[0].providerId,
    user: state.firebase.profile
 })
 
-const SettingDashBoard = ({ updatePassword,providerId, user }) => {
+const SettingDashBoard = ({ updatePassword,providerId, user,updateProfile }) => {
   return (
     <Grid>
       <Grid.Column width={12}>
         <Switch>
           <Redirect exact from="/Settings" to="/Settings/BasicPage" />
-          <Route path="/Settings/BasicPage" render={()=> <BasicPage initialValues={user}/>} />
+          <Route path="/Settings/BasicPage" render={()=> <BasicPage updateProfile={updateProfile} initialValues={user}/>} />
           <Route path="/Settings/AboutPage" component={AboutPage} />
           <Route path="/Settings/PhotoPage" component={PhotoPage} />
           <Route
