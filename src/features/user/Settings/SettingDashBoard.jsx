944bd938a8c212a04a9176094b493d6b22d753a8
Tayyab Chohahn <tayyabchohan7@gmail.com>
2018-10-23 22:44:48 +0500

    Adding the about me page

--- src/features/user/Settings/SettingDashBoard.jsx
+++ src/features/user/Settings/SettingDashBoard.jsx
@@ -8,28 +8,48 @@ import AcountPage from "./AcountPage";
 import { connect } from "react-redux";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { updatePassword } from "../../auth/AuthActions";
-import { updateProfile } from '../../user/userAction'
+import { updateProfile } from "../../user/userAction";
 const actions = {
   updatePassword,
   updateProfile
 };
-const mapState=(state)=>({
-   providerId: state.firebase.auth.providerData[0].providerId,
-   user: state.firebase.profile
-})
+const mapState = state => ({
+  providerId: state.firebase.auth.providerData[0].providerId,
+  user: state.firebase.profile
+});
 
-const SettingDashBoard = ({ updatePassword,providerId, user,updateProfile }) => {
+const SettingDashBoard = ({
+  updatePassword,
+  providerId,
+  user,
+  updateProfile
+}) => {
   return (
     <Grid>
       <Grid.Column width={12}>
         <Switch>
           <Redirect exact from="/Settings" to="/Settings/BasicPage" />
-          <Route path="/Settings/BasicPage" render={()=> <BasicPage updateProfile={updateProfile} initialValues={user}/>} />
-          <Route path="/Settings/AboutPage" component={AboutPage} />
+          <Route
+            path="/Settings/BasicPage"
+            render={() => (
+              <BasicPage updateProfile={updateProfile} initialValues={user} />
+            )}
+          />
+          <Route
+            path="/Settings/AboutPage"
+            render={() => (
+              <AboutPage updateProfile={updateProfile} initialValues={user} />
+            )}
+          />
           <Route path="/Settings/PhotoPage" component={PhotoPage} />
           <Route
             path="/Settings/AcountPage"
-            render={() => <AcountPage updatePassword={updatePassword} providerId={providerId} />}
+            render={() => (
+              <AcountPage
+                updatePassword={updatePassword}
+                providerId={providerId}
+              />
+            )}
           />
         </Switch>
       </Grid.Column>
