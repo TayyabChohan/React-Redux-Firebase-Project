4fb103f30d63b85e36b162764eebd82d2a274d94
TayyabChohan <33210220+TayyabChohan@users.noreply.github.com>
2018-12-10 14:34:55 +0500

    Creating our first cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
 const functions = require('firebase-functions');
+const admin=require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.createActivity=functions.firestore
+.document('events/{eventId}')
+.onCreate(event =>{
+  let newEvent=event.data();
+  console.log(newEvent);
+})
+
