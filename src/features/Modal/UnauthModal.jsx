caeadb7ea4b3c1d651d12ea7262cdf2cfb754b75
TayyabChohan <33210220+TayyabChohan@users.noreply.github.com>
2018-12-30 18:58:32 +0500

    Adding a popup for anonymous users when clicking buttons

--- src/features/Modal/UnauthModal.jsx
+++ src/features/Modal/UnauthModal.jsx
@@ -8,8 +8,14 @@ const actions = {closeModal, openModal};
 
 class UnauthModal extends Component {
     handleCloseModal=()=>{
-        this.props.history.goBack();
-        this.props.closeModal();
+        if(this.props.location.pathname.includes('/event')){
+            this.props.closeModal(); 
+        }
+        else{
+            this.props.history.goBack();
+            this.props.closeModal();
+        }
+       
     }
     render() {
         const {openModal} = this.props;
